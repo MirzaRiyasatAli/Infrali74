2dc9807a7c49704e034090bcccd7a496c0b5439e
MirzaRiyasatAli <67826255+MirzaRiyasatAli@users.noreply.github.com>
2021-05-09 21:05:58 +0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="Infrali74",
-    version="0.0.1",
-    author="Mirza Riyasat Ali",
-    author_email="mirzariyasatali1@gmail.com",
-    description="A vein detector library",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/MirzaRiyasatAli/MirzaRiyasatAli.git",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+from distutils.core import setup
+setup(
+  name = 'Infrali74',         # How you named your package folder (MyLib)
+  packages = ['Infrali74'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  author = 'Mirza Riyasat Ali',                   # Type in your name
+  author_email = 'mirzariyasatali1@domain.com',      # Type in your E-Mail
+  url = 'https://github.com/MirzaRiyasatAli/Infrali74.git',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['Veinsdetecting', 'Python', 'Veinfinder'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
