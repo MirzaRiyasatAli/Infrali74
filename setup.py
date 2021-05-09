48f1a4a2787c79312f74ae2db6627aa442e7fe0f
MirzaRiyasatAli <67826255+MirzaRiyasatAli@users.noreply.github.com>
2021-05-09 20:36:05 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Infrali74",
+    version="0.0.1",
+    author="Mirza Riyasat Ali",
+    author_email="mirzariyasatali1@gmail.com",
+    description="A vein detector library",
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
