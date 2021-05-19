37d688d74c64428972061225dff997dc45d382cd
MirzaRiyasatAli <67826255+MirzaRiyasatAli@users.noreply.github.com>
2021-05-19 20:00:27 +0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from distutils.core import setup
 setup(
-  name = 'Infrali74',         # How you named your package folder (MyLib)
-  packages = ['Infrali74'],   # Chose the same as "name"
+  name = 'Infrali074',         # How you named your package folder (MyLib)
+  packages = ['Infrali074'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Veindetection helper Function',   # Give a short description about your library
   author = 'Mirza Riyasat Ali',                   # Type in your name
   author_email = 'mirzariyasatali1@domain.com',      # Type in your E-Mail
   url = 'https://github.com/MirzaRiyasatAli/Infrali74.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['Veinsdetecting', 'Python', 'Veinfinder', 'Veinlocator', 'Veindetection'],   # Keywords that define your package best
+  download_url = 'https://github.com/MirzaRiyasatAli/Infrali74.git',    # I explain this later on
+  keywords = ['Veindetecting', 'Python', 'Veinfinder', 'Veinlocator', 'Veindetection'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'opencv-python',
           'numpy',
           'matplotlib',
+          'os',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
